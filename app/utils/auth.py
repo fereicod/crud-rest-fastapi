be389806b6b5c8467de181ac0e0b30359d997272
Fernando Espino <mferna.92@gmail.com>
2025-08-13 13:21:52 -0600

    Update add typing

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta, timezone
 from jwt import encode
 from app.core.config import settings
 
-SECRET_KEY = settings.JWT_SECRET
-ALGORITHM = settings.JWT_ALGORITHM
-EXPIRE_MINUTES = settings.JWT_EXPIRE_MINUTES
+SECRET_KEY: str =  settings.JWT_SECRET
+ALGORITHM: str =  settings.JWT_ALGORITHM
+EXPIRE_MINUTES: str =  settings.JWT_EXPIRE_MINUTES
 
 def expirate_token(expiration_time: str) -> datetime:
     return datetime.now(timezone.utc) + timedelta(minutes=int(expiration_time))
