58995afac7fe6dc54a461adc7335a67e8df5d01f
Fernando Espino <mferna.92@gmail.com>
2025-08-13 11:48:09 -0600

    Update schema to Admin login, Admin and Product

--- app/schemas/api_schema.py
+++ app/schemas/api_schema.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel
 
-class Admin(BaseModel):
+class AdminLogin(BaseModel):
     username: str
     password: str
+
+class Admin(BaseModel):
+    id: int
+    username: str
+    email: str
+
+class Product(BaseModel):
+    sku: str
+    name: str
+    price: float
+    brand: str
 
 class Token(BaseModel):
     access_token: str
