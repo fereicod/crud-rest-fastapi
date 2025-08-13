a44425b03b52f9a4207ca5b7faf9c85f5ade26c8
Fernando Espino <mferna.92@gmail.com>
2025-08-13 11:49:03 -0600

    Add route product init

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import admin
+from app.routers import admin, product
 
 app = FastAPI(title="Zebrands")
 
 app.include_router(admin.router, prefix="/admin")
+app.include_router(product.router, prefix="/product")
 
 @app.get('/')
 def root():
