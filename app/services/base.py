a0c8a6cfcc9239f8222ab76904f865761798ed68
Fernando Espino <mferna.92@gmail.com>
2025-08-14 13:56:14 -0600

    Update base services handler

--- app/services/base.py
+++ app/services/base.py
@@ -1,15 +1,20 @@
+from abc import ABC, abstractmethod
 from sqlmodel import Session
-from app.repositories.admim_repository import AdminRepository
-from app.services.admin_services import AdminService
 
-class BaseServiceHandler:    
+class BaseServiceHandler(ABC):    
     def __init__(self, session: Session):
         self.session = session
         self._setup_repositories()
         self._setup_services()
     
+    @abstractmethod
     def _setup_repositories(self):
-        self.admin_repo = AdminRepository(self.session)
+        pass
     
+    @abstractmethod
     def _setup_services(self):
-        self.admin_service = AdminService(self.admin_repo)
+        pass
+    
+    @abstractmethod
+    def get_services(self):
+        pass
