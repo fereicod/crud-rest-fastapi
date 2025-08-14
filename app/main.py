6837a2b97a5ae12463cfb057b6de34f8af030d0f
Fernando Espino <mferna.92@gmail.com>
2025-08-13 21:33:22 -0600

    Add versioning

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.routers import admin, product
 
 app = FastAPI(title="Zebrands")
 
-app.include_router(admin.router, prefix="/admin")
-app.include_router(product.router, prefix="/product")
+versionAPI = "v1"
+app.include_router(admin.router, prefix=f"/api/{versionAPI}/admin")
+app.include_router(product.router, prefix=f"/api/{versionAPI}/product")
+
 
