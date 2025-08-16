ab381b1cb8063673ef086fc6f0cc62284b4f3768
Fernando Espino <mferna.92@gmail.com>
2025-08-16 12:09:05 -0600

    Update include services handlet to product

--- app/services/services_handler.py
+++ app/services/services_handler.py
@@ -1,6 +1,9 @@
 from app.services.base import BaseServiceHandler
 from app.providers.admin_provider import AdminProvider
+from app.providers.product_provider import ProductProvider
 from app.services.admin_services import AdminService
+from app.services.product_services import ProductService
+
 
 class AdminServiceHandler(BaseServiceHandler):
 
@@ -12,3 +15,14 @@ class AdminServiceHandler(BaseServiceHandler):
     
     def get_services(self) -> AdminService:
         return self.admin_service
+
+class ProductServiceHandler(BaseServiceHandler):
+
+    def _setup_repositories(self) -> None:
+        self.product_repo = ProductProvider(self.session)
+    
+    def _setup_services(self) -> None:
+        self.product_service = ProductService(self.product_repo)
+    
+    def get_services(self) -> ProductService:
+        return self.product_service
