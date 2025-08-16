f1124d1ac90f041f47b9c1434aed4178ee219ad3
Fernando Espino <mferna.92@gmail.com>
2025-08-16 12:10:24 -0600

    Update notifier

--- app/utils/notifier.py
+++ app/utils/notifier.py
@@ -44,13 +44,13 @@ def send_email(email: str, subject: str, body: str) -> None:
     # ToDo: Integrate SENDGRID, SMTPLIB, etc
     print(f"Sending email to {email} | Subject: {subject} | Body: {body}")
 
-def notify_admins(product: dict, user: str, background_tasks: BackgroundTasks):
+def notify_admins(product_sku: str, user: str, background_tasks: BackgroundTasks):
     other_admins = [u for u in db_users.values() if u["username"] != user]
     for admin in other_admins:
         background_tasks.add_task(partial(
                 send_email,
                 admin["email"], 
-                f"Product {product['sku']} updated", 
+                f"Product {product_sku} updated", 
                 "Details..."
             ))
 
