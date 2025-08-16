32b9f1ff2656d146f6c5b3bf728f146fd09d6f89
Fernando Espino <mferna.92@gmail.com>
2025-08-16 12:05:49 -0600

    Update models to sql

--- app/database/models.py
+++ app/database/models.py
@@ -2,6 +2,15 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
+class Admin(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    password: str
+    email: Optional[str] = None
+    is_active: bool = True
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
 class Product(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     sku: str = Field(index=True, unique=True)
@@ -11,9 +20,4 @@ class Product(SQLModel, table=True):
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
     
-class Admin(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    password: str
-    email: Optional[str] = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+
