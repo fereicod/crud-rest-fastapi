087cc73248194e6afeaeace114ee308b34e7dd6c
Fernando Espino <mferna.92@gmail.com>
2025-08-16 12:08:07 -0600

    Update provder product to add CRUD operations

--- app/providers/product_provider.py
+++ app/providers/product_provider.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, select
 from typing import Optional
 from app.database.models import Product
 
-class ProductRepository:
+class ProductProvider:
     def __init__(self, session: Session):
         self.session = session
     
@@ -10,3 +10,22 @@ class ProductRepository:
         statement = select(Product).where(Product.sku == sku)
         return self.session.exec(statement).first()
 
+    def get_all(self) -> list[Product]:
+        statement = select(Product)
+        return list(self.session.exec(statement))
+
+    def create(self, product: Product) -> Product:
+        self.session.add(product)
+        self.session.commit()
+        self.session.refresh(product)
+        return product
+    
+    def update(self, product: Product) -> Product:
+        self.session.add(product)
+        self.session.commit()
+        self.session.refresh(product)
+        return product
+
+    def delete(self, product: Product):
+        self.session.delete(product)
+        self.session.commit()
