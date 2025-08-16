975dcf0dec337393648743cebfa280031718d8d2
Fernando Espino <mferna.92@gmail.com>
2025-08-16 12:09:29 -0600

    Update expose services product

--- app/services/services.py
+++ app/services/services.py
@@ -2,9 +2,15 @@ from fastapi import Depends
 from sqlmodel import Session
 from app.database.manager import get_session
 from app.services.services_handler import AdminServiceHandler
+from app.services.services_handler import ProductServiceHandler
 from app.services.admin_services import AdminService
+from app.services.product_services import ProductService
 
 
 def get_admin_services(session: Session = Depends(get_session)) -> AdminService:
     handler = AdminServiceHandler(session)
+    return handler.get_services()
+
+def get_product_services(session: Session = Depends(get_session)) -> ProductService:
+    handler = ProductServiceHandler(session)
     return handler.get_services()
