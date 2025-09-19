741bbb1d972e4cd19dcbd61f2ef29d263d4d7894
Fernando Espino <mferna.92@gmail.com>
2025-09-18 18:05:49 -0600

    Include middleware with origins

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from app.routers import admin, product
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Zebrands")
 
-versionAPI = "v1"
-app.include_router(admin.router, prefix=f"/api/{versionAPI}/admin")
-app.include_router(product.router, prefix=f"/api/{versionAPI}/product")
+origins = [
+    "http://localhost:5173", # Origin React app
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+version_api_v1 = "v1"
+app.include_router(admin.router, prefix=f"/api/{version_api_v1}/admin")
+app.include_router(product.router, prefix=f"/api/{version_api_v1}/product")
